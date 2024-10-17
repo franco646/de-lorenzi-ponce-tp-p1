243fe441a2d5d82c2ed7ab2b27adbcd50d571d31
Franco De Lorenzi <francodl646@gmail.com>
2024-10-17 16:47:01 +0000

    Revert "Corrige gravedad y colisiones"
    This reverts commit 8d335529c8c552e6f1a8a5112aa79df71588b263

--- src/juego/Colisiones.java
+++ src/juego/Colisiones.java
@@ -1,6 +1,5 @@
 package juego;
 
-<<<<<<< Upstream, based on origin/master
 public class Colisiones {
 	
 	//clase para colisionesss
@@ -64,47 +63,4 @@ public static boolean is_on_floor(Gnomo gno , Isla isla) {
 		
 	}
 	
-=======
-import java.awt.Rectangle;
-
-public class Colisiones {
-
-	// clase para colisionesss
-
-	public static boolean colisionan(Rectangle caja1, Rectangle caja2) {
-		int bordeDerCaja1 = caja1.x + caja1.width / 2;
-		int bordeIzqCaja1 = caja1.x - caja1.width / 2;
-		int bordeSupCaja1 = caja1.y + caja1.height / 2;
-		int bordeInfCaja1 = caja1.y - caja1.height / 2;
-
-		int bordeDerCaja2 = caja2.x + caja2.width / 2;
-		int bordeIzqCaja2 = caja2.x - caja2.width / 2;
-		int bordeSupCaja2 = caja2.y + caja2.height / 2;
-		int bordeInfCaja2 = caja2.y - caja2.height / 2;
-
-		boolean colizionHorizontal = bordeDerCaja1 > bordeIzqCaja2 && bordeIzqCaja1 < bordeDerCaja2;
-		boolean colisionVertical = bordeSupCaja1 > bordeInfCaja2 && bordeInfCaja1 < bordeSupCaja2;
-
-		return colizionHorizontal && colisionVertical;
-
-	}
-
-	// is_on_floor detecta si el objeto esta tocando el suelo :::
-
-	public static boolean estaSobreIsla(Rectangle caja, Isla isla) {
-		int bordeInfCaja = caja.y + caja.height / 2;
-
-		double bordeDerIsla = isla.x + isla.ancho / 2;
-		double bordeIzqIsla = isla.x - isla.ancho / 2;
-		double bordeSupIsla = isla.y - isla.alto / 4; // Es un poco mas abajo del borde superior para que lo que esté
-														// encima no parezca flotando
-
-		boolean colisionHorizontal = caja.x > bordeIzqIsla && caja.x < bordeDerIsla;
-		boolean colisionVertical = Math.abs(bordeInfCaja - bordeSupIsla) <= 5;
-
-		return colisionHorizontal && colisionVertical;
-
-	}
-
->>>>>>> 16185b6 Corrige gravedad y colisiones
 }
